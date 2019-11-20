949554ae6f4ec475565973bead4dd2b0d4c5f735
Jorge Alvarez <j.alvarz@hotmail.com>
2019-11-19 17:42:45 -0800

    Setup jest

---
+++ components/test/feed.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Post, Feed } from "../feed";
+
+describe("Post Component", () => {
+  test("Should match the snapshot", () => {});
+  test("Should render even if an argument is missing", () => {});
+});
+
+describe("Feed component", () => {
+    test("Should match the snapshot", () => {});
+    test("Should render a no feed message when there are no posts", () => {});
+    test("Should render even if an argument is missing", () => {});
+});
