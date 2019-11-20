98e589c802fed6eaa0b3f3de985dba7e31a30905
Jorge Alvarez <j.alvarz@hotmail.com>
2019-11-19 23:33:04 -0800

    Add component for creating posts

--- components/feed.js
+++ components/feed.js
@@ -8,7 +8,6 @@ import PropTypes from "prop-types";
  * @param title: The title of the post
  * @param className: The prefixed class name for the component
  * @returns React: FunctionalComponent
- * @constructor
  */
 export const Post = ({
   author = "",
@@ -31,6 +30,68 @@ Post.propTypes = {
 };
 
 /**
+ * Creates the UI for creating a new post
+ * @param onSubmit: The callback for the submit action
+ * @param id: The id used for the elements of the form
+ * @param className: The prefixed class name for the component
+ * @returns React: FunctionalComponent
+ */
+
+export const CreatePost = ({
+  onSubmit = () => {},
+  id = "create-post",
+  className = "create-post"
+}) => {
+  return (
+    <form id={id} className={`${className}__container`}>
+      <div className={`${className}__user-wrapper`}>
+        <label className={`${className}__user-label`} htmlFor={`${id}-user`}>
+          User:
+        </label>
+        <input
+          type="text"
+          name={`${id}-user`}
+          className={`${className}__user-input`}
+        />
+      </div>
+      <div className={`${className}__title-wrapper`}>
+        <label className={`${className}__title-label`} htmlFor={`${id}-title`}>
+          Title:
+        </label>
+        <input
+          type="text"
+          name={`${id}-title`}
+          className={`${className}__title-input`}
+        />
+      </div>
+      <div className={`${className}__body-wrapper`}>
+        <label className={`${className}__body-label`} htmlFor={`${id}-body`}>
+          Content:
+        </label>
+        <textarea
+          name={`${id}-body`}
+          className={`${className}__body-textarea`}
+          cols="3"
+        />
+      </div>
+      <div className={`${className}__submit-wrapper`}>
+        <input
+          className={`${className}__submit-input`}
+          type="submit"
+          onClick={onSubmit}
+        />
+      </div>
+    </form>
+  );
+};
+
+CreatePost.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  className: PropTypes.string
+};
+
+/**
  * Renders an array of posts using the Post component
  * @param posts: The data to be rendered
  * @param loading: Flag to set if the data is still loading
@@ -38,7 +99,6 @@ Post.propTypes = {
  * @param className: A prefix for the class name of this component
  * @param noFeedMessage: The message to be shown to the user if there are no posts
  * @returns React: FunctionalComponent
- * @constructor
  */
 export const Feed = ({
   posts = [],
